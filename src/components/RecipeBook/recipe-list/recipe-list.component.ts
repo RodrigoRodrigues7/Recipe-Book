1a8ac6ff56d818ca6699aade6b7da1dc6c67dad1
RodrigoRodrigues7 <rodrigo7.carvalho.rodrigues@gmail.com>
2019-05-02 16:02:03 -0300

    Added layout to the list of recipes and prepared to display the recipe details

--- src/components/RecipeBook/recipe-list/recipe-list.component.ts
+++ src/components/RecipeBook/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit { 
+  
+  recipesList: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://d9hyo6bif16lx.cloudfront.net/live/img/production/detail/menu/lunch-dinner_burgers_all-american-burger.jpg'),
+    new Recipe('Another Test Recipe', 'This is funny', 'https://d9hyo6bif16lx.cloudfront.net/live/img/production/detail/menu/lunch-dinner_burgers_all-american-burger.jpg'),
+    new Recipe('Yet another Test Recipe', 'This is getting dull ...', 'https://d9hyo6bif16lx.cloudfront.net/live/img/production/detail/menu/lunch-dinner_burgers_all-american-burger.jpg'),
+  ];
 
+  
   constructor() { }
 
   ngOnInit() {
   }
-
 }
